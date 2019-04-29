a4047395ad0dd5a062e0d948d0b4246309f51a82
xushaohua <henryxm@163.com>
2019-04-29 12:02:36 +0800

    添加开发注释

--- app/src/main/java/cn/nutch/plugindemo/DemoApp.java
+++ app/src/main/java/cn/nutch/plugindemo/DemoApp.java
@@ -7,11 +7,34 @@ import com.lzh.router.RouterRuleCreator;
 
 import cn.nutch.replugin.starter.Starter;
 
+/**
+ * ##:3 添加路由协议, 路由协议需要到平台创建添加，具有唯一性，不能与平台现有的协议重复
+ *    开发时可以自定义。
+ */
 @RouteConfig(baseUrl = BuildConfig.pluginAliasName + "://")
 public class DemoApp extends Application {
+
+    /**
+     * appKey and appSecret is useful when read data from platform backend server.
+     * @appKey
+     * @appSecret
+     *  这个两个值来自于在平台中创建的APP中，且是平台自动生成的
+     *  appKey创建后不能更改，appSecret只能重置，不能手动修改
+     *  这两个值只用于当插件要读取后台一些配置信息的时候有用，缺省可以不需要
+     */
+
+    private static String appKey = "Your app key";
+    private static String appSecret = "Your app secret";
+
     public void onCreate() {
         super.onCreate();
-        Starter.init(this, new RouterRuleCreator());
+
+        /**
+         * ##:4 初始化插件信息
+         *    RouterRuleCreator 为自动生成的 class.
+         *    编译未生成时，可先注释掉下面的行，避免编译出错
+         */
+        Starter.init(this, new RouterRuleCreator(), appKey, appSecret);
     }
 
 }
--- app/src/main/java/cn/nutch/plugindemo/MainActivity.java
+++ app/src/main/java/cn/nutch/plugindemo/MainActivity.java
@@ -7,17 +7,38 @@ import com.lzh.compiler.parceler.Parceler;
 import com.lzh.compiler.parceler.annotation.Arg;
 import com.lzh.nonview.router.anno.RouterRule;
 
-
+/**
+ * ##:5 添加路由入口，该值用于路由框架注册和发现该activity，host就就是通过该入口启动activity的
+ *      这也是插件的入口，没有这个，Host将不知道如何启动插件
+ *      插件入口需要定义至少一个，也可以无限多个。总之，只要告诉 Host 一个启动地址就可以了
+ *
+ *      为避免不必要的兼容问题，统一规定所有的插件activity需继承：AppCompatActivity
+ */
 @RouterRule("main")
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * 插件在通过路由方式进行访问是可以携带参数，参数名称和参数个数没有限制，自定义接收
+     *
+     * 如：demo://main?arg=hello&arg1=world
+     *
+     * 在 onCreate 中调用 ：Parceler.toEntity(this, getIntent());
+     * 参数值将被自动注入进来，不需要添加其他代码
+     * 参数仅支持字符串形式
+     */
     @Arg
     private String arg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /**
+         * 下面这行是解析参数值的调用方式
+         * 通过调用下面的方法，参数将自动获取
+         */
         Parceler.toEntity(this, getIntent());
+
         setContentView(R.layout.activity_main);
     }
 }
